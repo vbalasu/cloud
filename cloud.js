192ea29fcb12e0a89c8137608517a4a8c85b2aa3
vbalasu <vbalasu@gmail.com>
2018-02-25 10:35:21 -0800

    updated Azure functions

--- cloud.js
+++
@@ -1,1 +0,0 @@
-/* Server side methods for handling CSV files on Azure Blob Storage */
---
+++ cloudPut.js
@@ -0,0 +1,23 @@
+//https://github.com/vbalasu/cloud/cloudPut.js
+module.exports = function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    var filename = req.query.filename?req.query.filename:req.body.filename
+    var domain = req.query.domain?req.query.domain:req.body.domain
+    var contents = req.query.contents?req.query.contents:req.body.contents
+    if (filename) {
+        var azure = require('azure-storage')
+        var blobSvc = azure.createBlobService()
+        blobSvc.createAppendBlobFromText('blobcontainer', domain+'/'+filename, contents, function(error, result, response){
+            context.res = { body : 'https://cloudmaticafunctions.azurewebsites.net/api/cloudGet?code=m8Qpr9D0D/FyOatpL9jnCL34ZBtMBz1U04kp4n4dia9kAfhFjNWgVQ==&filename='+encodeURIComponent(filename)+'&domain='+encodeURIComponent(domain) }
+            context.done()
+        })
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass a filename and domain on the query string or in the request body"
+        };
+        context.done()
+    }
+};

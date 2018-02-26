835360df2be1a59631085b18e0f71bede379a890
vbalasu <vbalasu@gmail.com>
2018-02-26 06:12:02 -0800

    Working version

--- cloudPut.js
+++ cloudPut.js
@@ -8,7 +8,8 @@ module.exports = function (context, req) {
         var azure = require('azure-storage')
         var blobSvc = azure.createBlobService()
         blobSvc.createAppendBlobFromText('blobcontainer', domain+'/'+filename, contents, function(error, result, response){
-            context.res = { body : 'https://cloudmaticafunctions.azurewebsites.net/api/cloudGet?code=m8Qpr9D0D/FyOatpL9jnCL34ZBtMBz1U04kp4n4dia9kAfhFjNWgVQ==&filename='+encodeURIComponent(filename)+'&domain='+encodeURIComponent(domain) }
+            context.res = { headers: {"Content-Type": "text/plain"},
+                body : 'https://cloudmaticafunctions.azurewebsites.net/api/cloudGet?code=m8Qpr9D0D/FyOatpL9jnCL34ZBtMBz1U04kp4n4dia9kAfhFjNWgVQ==&filename='+encodeURIComponent(filename)+'&domain='+encodeURIComponent(domain) }
             context.done()
         })
     }

c7472c3cdd96acb8ed3284a0c8ad78fd6b568bcf
vbalasu <vbalasu@gmail.com>
2018-03-03 07:04:24 -0800

    Working version

--- cloudAppend.js
+++ cloudAppend.js
@@ -8,7 +8,7 @@ module.exports = function (context, req) {
         var azure = require('azure-storage')
         var blobSvc = azure.createBlobService()
         blobSvc.appendFromText('blobcontainer', domain+'/'+filename, contents, function(error, result, response){
-            if(error) context.log(error)
+            if(error) { context.res = {status:404,headers: {"Content-Type": "text/html"},body:error.message}; context.done() }
             else {
                 context.res = { headers: {"Content-Type": "text/html"},
                     body : 'https://cloudmaticafunctions.azurewebsites.net/api/cloudGet?code=m8Qpr9D0D/FyOatpL9jnCL34ZBtMBz1U04kp4n4dia9kAfhFjNWgVQ==&filename='+encodeURIComponent(filename)+'&domain='+encodeURIComponent(domain) }
